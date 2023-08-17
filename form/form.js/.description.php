c4f0de011559dea2a1aaf1830e979eea1ec123e0
vladkurr <vkakachko@mail.ru>
2023-08-17 10:13:19 +0300

    first commit

---
+++ form/form.js/.description.php
@@ -0,0 +1,15 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); $arComponentDescription = array(
+    "NAME" => GetMessage("Форма на чистом js"),
+);
+?>
+
+<!--вызов компонента-->
+<?// $APPLICATION->IncludeComponent(
+//    "form:form.js",
+//    ".default",
+//    Array(
+//'MAIL_EVENT' => 'FORM_SENDED',
+
+//    ),
+//    false
+//);?>
