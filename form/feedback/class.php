4579bfa95cc097393fc132c9c0c35f011eb251b6
vladkurr <vkakachko@mail.ru>
2024-02-09 15:04:28 +0300

    add b24

--- form/feedback/class.php
+++ form/feedback/class.php
@@ -1,4 +1,7 @@
 <?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/*var SITE_ID */
+
 class Form extends CBitrixComponent
 {
     private function normalizeFiles($name, $vector) // создание удобного массива файлов
@@ -14,31 +17,71 @@ class Form extends CBitrixComponent
         return $result;
     }
 
+    private function leadCreate()
+    {
+        $data = $_POST;
+
+        // соответствие полей
+        $data["EMAIL"] = $data["MAIL"];
+        $data["COMMENT"] = $_POST["MESSAGE"] ? "Доп. информация: " . $_POST["MESSAGE"] . "<br>" : "";
+        $data["COMMENT"] .= 'ссылка на страницу - ' . $data["PAGE"];
+
+        $queryData = http_build_query(array(
+            'fields' => array(
+                "TITLE" => 'Заполнена форма на сайте investsochi.ru', //Заголовок лида
+                "SOURCE_ID" => 'WEB', //Источник лида
+                "NAME" => $data['NAME'] ? $data['NAME'] : 'Имя не заполнено', //Имя контакта
+                "EMAIL" => [["VALUE" => $data['EMAIL'], "VALUE_TYPE" => "WORK"]], //Почта контакта
+                "PHONE" => [["VALUE" => $data['PHONE'], "VALUE_TYPE" => "WORK"]], //Телефон контакта
+                "COMMENTS" => $data["COMMENT"],
+            ),
+            'params' => array("REGISTER_SONET_EVENT" => "Y"), //Говорим, что требуется зарегистрировать новое событие и оповестить всех прчиастных
+        ));
+
+        //обращаемся к Битрикс24 при помощи функции curl_exec
+        //метод crm.lead.add.json добавляет лид
+        $rest = 'crm.lead.add.json';
+
+        //url берется из созданного вебхука, удалив в нем окончание prifile/
+        //и добавив метод $rest на добавление лида
+        $queryUrl = 'https://investsochi.bitrix24.ru/rest/140/y4sjwv9qaqsj0dh4/' . $rest;
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $queryUrl,
+            CURLOPT_POSTFIELDS => $queryData,
+        ));
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+        $result = json_decode($result, 1);
+
+        if (array_key_exists('error', $result)) {
+            echo "Ошибка при сохранении лида: " . $result['error_description'] . "";
+        } else {
+            echo $result['result'];
+        }
+    }
+
     private function mailer($id) // отправка почты через mail() и активация почтового шаблона
     {
         if ($this->arParams["MAIL_EVENT"] != null) {
+            global $APPLICATION;
             foreach ($this->arParams["MAIL_EVENT"] as $event) {
-                $urlInAdmin = 'http://'. $_SERVER["SERVER_NAME"] .'/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=' . $this->arParams['IBLOCK_ID'] . '&type=Forms&lang=ru&ID=' . $id . '&find_section_section=-1&WF=Y';
+                $urlInAdmin = 'http://' . $_SERVER["SERVER_NAME"] . '/bitrix/admin/iblock_element_edit.php?IBLOCK_ID=' . $this->arParams['IBLOCK_ID'] . '&type=Forms&lang=ru&ID=' . $id . '&find_section_section=-1&WF=Y';
+                $_POST["PAGE"] = "https://" . $_SERVER["SERVER_NAME"] . $APPLICATION->GetCurPage();
                 $_POST["ADMIN_URL"] = $urlInAdmin;
                 echo CEvent::Send($event, SITE_ID, $_POST);
             }
-        }
-        if ($this->arParams["MAIL_TO"] != null) {
-            $to = $this->arParams["MAIL_TO"];
-            $subject = 'На сайте заполнена форма';
-            $message = '';
-            foreach ($this->request->getPostList() as $key => $post) {
-                if ($key != "TOKEN")
-                    $message .= $key . ": " . $post . "\n";
-            }
-            echo mail($to, $subject, $message);
         }
     }
 
     private function addToIblock() // добавление в инфоблок
     {
         if ($this->arParams["IBLOCK_ID"] != null) {
-
             $el = new CIBlockElement;
             $propertyValues = $this->request->getPostList()->getValues();
             foreach ($_FILES as $key => $file) {
@@ -52,8 +95,8 @@ class Form extends CBitrixComponent
                 'IBLOCK_ID' => $this->arParams["IBLOCK_ID"],
                 "IBLOCK_TYPE" => $this->arParams["IBLOCK_TYPE"],
                 "CODE" => date('l jS \of F Y h:i:s A'),
-                "NAME" => "На сайте заполнена форма " . $this->arParams["TOKEN"],
-                "ACTIVE" => "N",
+                "NAME" => "На сайте заполнена форма",
+                "ACTIVE" => "Y",
                 "PROPERTY_VALUES" => $propertyValues,
             );
             $res = $el->Add($arLoadProductArray, false, false, true);
@@ -63,11 +106,16 @@ class Form extends CBitrixComponent
 
     public function executeComponent() // инициализация компонента
     {
+        // если есть $_POST - компонент не инициализирует форму, запускает ее отправку
         if ($this->request->getPost("TOKEN") == $this->arParams["TOKEN"]) {
             $GLOBALS["APPLICATION"]->RestartBuffer(); // строка для удобства отладки
-            $elemId = $this->addToIblock();
-            $this->mailer($elemId);
+            $id = $this->addToIblock(); // добавление в инфоблок
+            $this->mailer($id); // отправка почты
+            $this->leadCreate(); // создание лида (b24)
+
+            // file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/test.txt", serialize($_POST)); // debug
         } else {
+            // обычный запуск компонента
             $this->includeComponentTemplate();
         }
     }
