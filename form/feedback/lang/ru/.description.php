616598a12e8ff913a710389f21a5c8ee8ef5fee9
vladkurr <vkakachko@mail.ru>
2024-09-25 15:27:08 +0300

    add pre-version of module "settings"

--- form/feedback/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$MESS["COMPONENT_PATH_ID"] = "local";
-$MESS["COMPONENT_PATH_NAME"] = "form";
-$MESS["COMPONENT"] = "Форма обратной связи";
-$MESS["COMPONENT_DESCRIPTION"] = "";
